b36a535d2efa389210f93e972bbdeb0aebe3622a
Elijah Schneider <Jetvac2@icloud.com>
2025-07-03 12:26:15 -0400

    first commit

---
+++ core/src/main/java/com/Jetvac2/MicroJam/Main.java
@@ -0,0 +1,36 @@
+package com.Jetvac2.MicroJam;
+
+import com.badlogic.gdx.ApplicationListener;
+
+public class Main implements ApplicationListener {
+   
+    @Override
+    public void create() {
+     
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    
+    }
+
+    @Override
+    public void render() {
+    
+    }
+
+    @Override
+    public void pause() {
+        
+    }
+
+    @Override
+    public void resume() {
+        
+    }
+
+    @Override
+    public void dispose() {
+        
+    }
+}
